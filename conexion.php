0dd358d5da291da7423fee0a27ab38d64236780e
DiegoC01 <75624730+DiegoC01@users.noreply.github.com>
2022-06-06 02:22:30 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -4,10 +4,9 @@ $host = "ec2-54-227-248-71.compute-1.amazonaws.com";
 $usuario = "ewenxgtlkruflj";
 $pass = "3fbff6de5781b3a2a062ae8f4fd8947e015f909103689e3d8d56eddf52677912";
 $bd = "d5r5lnm9t8um47";
-$port = 5432;
 
 // CONEXION AL SISTEMA
-$conectar = mysqli_connect($host, $usuario, $pass ,$bd, $port);
+$conectar = mysqli_connect($host, $usuario, $pass ,$bd);
 mysqli_set_charset($conectar,"utf8");
 
 // Verificar conexión
